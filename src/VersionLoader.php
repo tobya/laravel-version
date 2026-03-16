246d5674005f9086463492df318d2e8c7d3422de
Toby Allen <toby@cookingisfun.ie>
2026-03-16 08:20:12 +0000

    write to either

--- src/VersionLoader.php
+++ src/VersionLoader.php
@@ -11,21 +11,56 @@ class VersionLoader
 {
     protected string $path;
 
+    protected string $storageType;
+
     public function __construct(?string $path = null)
     {
-        $this->path = $path ?? App::basePath('config/app.php');
+        $this->storageType = config('version.storage') ;
+
+        if ($this->storageType == 'json-file')
+        {
+          $this->path = $path ?? App::basePath('version.json');
+        }
+        else  if ($this->storageType == 'config-file')
+        {
+          $this->path = $path ?? App::basePath('config/version.php');
+        }
+
+
     }
 
     /**
-     * Load version from the version.json file.
+     * Load version from the storage file.
      */
     public function load(): Version
     {
-        if (config('app.version',null) != null )
-        {
-            return new Version(config('app.version') ?? '1.0.0');
-        }
 
+      if ($this->storageType == 'json-file')
+      {
+        return $this->loadFromJson();
+      }
+      else  // if ($this->storageType == 'config-file')
+      {
+        return $this->loadFromConfig();
+      }
+
+    }
+
+  /**
+   * Load version from Config file
+   * @return Version
+   */
+    public function loadFromConfig() : Version
+    {
+         return new Version(config('version.version') ?? '1.0.0');
+    }
+
+  /**
+   * Load version from version.json file.
+   * @return Version
+   */
+    public function loadFromJson() : Version
+    {
         if (File::exists($this->path)) {
             $data = json_decode(File::get($this->path), true);
 
@@ -39,7 +74,7 @@ class VersionLoader
     }
 
     /**
-     * Save the version to the version.json file.
+     * Save the version to the selected file.
      */
     public function save(Version $version): void
     {
@@ -56,6 +91,32 @@ class VersionLoader
         File::put($this->path, json_encode([
             'version' => $version->get(),
         ], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)."\n");
+    }
+
+    public function saveToConfig(Version $version)
+    {
+       $content = File::get($this->path);
+
+        // Find
+        // Version => '1.0.0'
+        // string in version config and replace
+        // PREG  'version'(\s*)=>(\s*)'(.*)'
+        $updated = $result = preg_replace(
+            '/\'version\'(\s*)=>(\s*)\'(.*)\'/',
+            "'version' => '". $version->get()."'",
+            $content);
+
+        File::put($this->path, $updated);
+
+    }
+
+    public function saveToJson(Version $version) : void
+    {
+
+        File::put($this->path, json_encode([
+            'version' => $version->get(),
+        ], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)."\n");
+
     }
 
     /**
